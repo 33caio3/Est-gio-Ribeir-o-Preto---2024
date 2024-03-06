83067cbaeef9376f8f8c38718e6c0c7e6af1a638
33caio3 <132357221+33caio3@users.noreply.github.com>
2024-03-06 14:32:18 -0300

    Update rp.c

--- RP/3/f/rp.c
+++ RP/3/f/rp.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+    //2,10, 12, 16, 17, 18, 19, ____  
+    //A seuqência é baseada se o número começa com a letra "d", logo será 200
+
 int main (){
     int num = 200;
     if(num == 2 || num == 10 || num >=12 && num <= 20 || num >=200 && num <=299){
