1f3100faf7a534a47934df1e53dfe37db903e3f9
33caio3 <132357221+33caio3@users.noreply.github.com>
2024-03-06 14:36:18 -0300

    Update rp.c

--- RP/4/rp.c
+++ RP/4/rp.c
@@ -1,1 +1,8 @@
 
+/*Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala 
+em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada? */
+
+/*Ligaria o primeiro interruptor e verificaria se minha lâmpada acendeu, se não acendeu vou a sala à minha direita, caso não esteja aceso, quer dizer que a sala a
+esquerda está acesa. Volto a minha sala e acendo o segundo interruptor e verifico se minha sala acendeu, se não acendeu quer dizer que a sala a direita
+está com a luz acesa, e o ultimo interruptor é a luz de minha sala.*/
